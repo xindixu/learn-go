952f2392c397b7dbd6b1bcfee063b58b0222acc9
Xindi Xu <xindixu0@gmail.com>
2022-01-21 17:47:24 -0500

    feat: random, array

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,12 +3,29 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("Empty name")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// Go executes init functions automatically at program startup, after global variables have been initialized.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
