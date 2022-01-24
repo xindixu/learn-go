31cd5c46ad3ce35914f3e7b407fec6af80e18a39
Xindi Xu <xindixu0@gmail.com>
2022-01-24 11:15:44 -0500

    feat: channels

---
+++ channels.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func sum(s []int, c chan int) {
+	sum := 0
+	time.Sleep(1000 * time.Millisecond)
+
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum // send sum to c
+}
+
+func main() {
+	s := []int{7, 2, 8, -9, 4, 0}
+	length := len(s)
+
+	/*
+		NOTE: Channels must be created before use
+		ch := make(chan int) // create channel ch with type int
+		ch <- v    // Send v to channel ch.
+		v := <-ch  // Receive from ch, and assign value to v.
+
+		Channel sends and receives until the other side is ready
+
+	*/
+	c := make(chan int)
+
+	fmt.Println("Summing...")
+
+	go sum(s[:length/2], c)
+	go sum(s[length/2:], c)
+	// these will wait for the result to comeback
+	x, y := <-c, <-c // receive from c
+
+	fmt.Println(x, y, x+y)
+}
